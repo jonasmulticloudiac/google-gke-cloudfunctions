26875f38291c3bede9bd43c8814ee8e717efc91b
root <root@DESKTOP-S7GGDE6.localdomain>
2021-03-23 08:17:02 -0300

    Change_provider

--- providers.tf
+++ providers.tf
@@ -1,6 +1,6 @@
 provider "google" {
   # version     = "2.7.0"
-  credentials = file("/google/auth/tcb-gcp2-moodle.json")
+  credentials = file("<caminho/Nome_do_seu_arquivo_json>")
   project     = var.project
   region      = var.region
 }
